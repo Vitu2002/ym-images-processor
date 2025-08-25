27bae84343db7eedb68312486f409aabcdf41d4a
Vitu2002 <contato@vitu2002.dev>
2025-08-26 01:43:42 +0200

    feat(image): add queue integration and chunk size handling in ImageScheduler to limit queue size

--- src/image/image.scheduler.ts
+++ src/image/image.scheduler.ts
@@ -1,13 +1,17 @@
+import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { Queue } from 'bullmq';
 import { MinioService } from 'src/minio/minio.service';
 import { ImageProducer } from './image.producer';
 
 @Injectable()
 export class ImageScheduler implements OnModuleInit {
     private readonly logger = new Logger(ImageScheduler.name);
+    private readonly CHUNK_SIZE = parseInt(`${process.env.CHUNK_SIZE}` || '100');
 
     constructor(
+        @InjectQueue('image-convert') private readonly queue: Queue,
         private readonly minio: MinioService,
         private readonly producer: ImageProducer
     ) {}
@@ -21,10 +25,23 @@ export class ImageScheduler implements OnModuleInit {
     @Cron(CronExpression.EVERY_HOUR)
     async handleCron() {
         this.logger.log('Running scheduler...');
+        const allowed = await this.checkForPendingJobs();
+        if (!allowed) return;
         const images = await this.minio.listImages();
         this.logger.log(`Found ${images.length} images`);
         for (const image of images) {
             await this.producer.addConversion(image);
         }
+    }
+
+    async checkForPendingJobs() {
+        const waiting = await this.queue.getWaitingCount();
+        const limit = Math.floor(this.CHUNK_SIZE / 2);
+
+        if (waiting > limit) {
+            this.logger.warn(`There are ${waiting} jobs waiting, skipping...`);
+            return false;
+        }
+        return true;
     }
 }
